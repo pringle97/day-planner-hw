8c067c439fd1584b1ca1746fa5c4d73e91661558
pringle97 <malia.pringle@gmail.com>
2021-12-11 19:49:38 -0800

    local storage (helped by tutor)

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-//using jquery to add btn function
+//using jquery to add btn function, adding txt to local storage
 $(".btn").on("click", function () {
   var id = $(this).attr("id").split("-")[0]
   var plan = $("#" + id + "-txt").val()
   console.log(id, plan)
   localStorage.setItem(id, plan)
 })
+
 //adding current date variable to see date on page
 var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a')
 $("#currentDay").text(currentDate)
 var currentHour = moment().hours()
 console.log(currentDate, currentHour)
 
+//calling txt from local storage and adding classes
+for (let i = 8; i < 19; i++) {
+  var chose = localStorage.getItem(i)
+  $("#" + i + "-txt").val(chose)
+  if (i < currentHour) {
+    $("#" + i + "-txt").addClass("past")
+  }
+  else if (i === currentHour) {
+    $("#" + i + "-txt").addClass("present")
+  }
+  else {
+    $("#" + i + "-txt").addClass("future")
+  }
+}
