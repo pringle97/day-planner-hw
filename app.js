b700b1ee51cf20a81831906a44c1789359911766
pringle97 <malia.pringle@gmail.com>
2021-12-11 19:34:35 -0800

    adding variables for date

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+//using jquery to add btn function
 $(".btn").on("click", function () {
   var id = $(this).attr("id").split("-")[0]
   var plan = $("#" + id + "-txt").val()
   console.log(id, plan)
   localStorage.setItem(id, plan)
 })
+//adding current date variable to see date on page
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a')
+$("#currentDay").text(currentDate)
+var currentHour = moment().hours()
+console.log(currentDate, currentHour)
+
